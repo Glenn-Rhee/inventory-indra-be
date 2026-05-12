d4e9bb169dbbee44ee7269ad31244a7fe1869e66
Glenn-Rhee <glennviktor5@gmail.com>
2026-05-12 17:01:50 +0700

    feat: setup struct for get product response

--- model/stock.go
+++ model/stock.go
@@ -4,6 +4,14 @@ import (
 	"time"
 )
 
+type StatusStock string
+
+const (
+	StatusStockSafe 	StatusStock = "SAFE"
+	StatusStockLow		StatusStock	= "LOW-STOCK"
+	StatusStockSoldOut	StatusStock	= "SOLD-OUT"
+)
+
 type Stock struct {
 	Id 				string 		`gorm:"primaryKey;type:varchar(255)"`
 	StockPerButir	int 		`gorm:"not null"`
@@ -11,4 +19,21 @@ type Stock struct {
 	ProductId 		string		`gorm:"type:varchar(255)"`
 
 	Product 		*Product	`gorm:"foreignKey:ProductId"`
+}
+
+type GetStocks struct {
+	TotalProduct 		int64
+	TotalLowStock		int
+	TotalProductExpired	int
+	Products			[]GetProductStock			 		
+}
+
+type GetProductStock struct {
+	Id				string
+	Name			string
+	Stock			int
+	StatusStock		StatusStock
+	StatusExpired	ExpiredStatus
+	ExpiredDate		time.Time
+	LastUpdate		time.Time	
 }
