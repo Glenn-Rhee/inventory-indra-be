5cb054a235d1744e81d0e3db461bdcb33b4dcb97
Glenn-Rhee <glennviktor5@gmail.com>
2026-05-13 22:45:19 +0700

    feat: added get transaction repository

--- repositories/transaction_repo.go
+++ repositories/transaction_repo.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"inventory-indra/model"
 	"log"
+	"math"
 	"net/http"
 	"time"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -35,15 +35,19 @@ func (r *TransactionRepository) CreateTransaction(data model.CreateTransaction) 
 		return errors.New("An error while create transaction! Please try again later"), http.StatusInternalServerError
 	}
 
+
+	createdAt := time.Now()
+	var idTransaction = createdAt.Format("TX-02012006-150405")
+
 	transaction := model.Transaction{
-		Id: uuid.New().String(),
+		Id: idTransaction,
 		ProductId: product.Id,
 		TransactionType: data.TransactionType,
 		Quantity: data.Quantity,
-		CreatedAt: time.Now(),
+		CreatedAt: createdAt,
 	}
 
-	result = tx.Model(&transaction)
+	result = tx.Model(&transaction).Create(transaction)
 	if result.Error != nil {
 		tx.Rollback()
 		log.Println("Error while create data transaction:", result.Error.Error())
@@ -68,4 +72,71 @@ func (r *TransactionRepository) CreateTransaction(data model.CreateTransaction) 
 
 	tx.Commit()
 	return nil, http.StatusCreated
+}
+
+type GetTransactionParams struct {
+	Limit 	int
+	Page 	int
+	Filter 	string
+}
+
+func (r *TransactionRepository) GetTransaction(params GetTransactionParams) (model.GetTransaction, error, int) {
+	var transactions []model.Transaction
+	var totalRows int64
+	offset := (params.Page - 1) * params.Limit
+	query := r.db.Model(&model.Transaction{}).
+		Joins("JOIN products ON products.id = transactions.product_id")
+
+	if params.Filter != "" {
+		query = query.Where(
+			"transactions.id = ? OR products.name ILIKE ? OR transactions.transaction_type::text ILIKE ?", 
+			params.Filter, 
+			"%" + params.Filter + "%",
+			"%" + params.Filter + "%",
+		)
+	}
+
+	query.Count(&totalRows)
+
+	result := query.Order("transactions.created_at ASC").
+				Limit(params.Limit).
+				Offset(offset).
+				Find(&transactions)
+
+	if result.Error != nil {
+		log.Println("Error while get data transaction:", result.Error.Error())
+		return model.GetTransaction{}, errors.New("An error while get data transaction! Please try again later!"), http.StatusInternalServerError
+	}
+
+	transactionsResponse := make([]model.TransactionResponse, len(transactions))
+	totalPages := int(math.Ceil(float64(totalRows) / float64(params.Limit)))
+	var totalRevenue = 0
+	var totalPurchase = 0
+	for i, transaction := range transactions {
+		totalPrice := transaction.Quantity * transaction.Product.PricePerButir
+		if transaction.TransactionType == model.TransactionIN {
+			totalRevenue += totalPrice
+		} else {
+			totalPurchase += totalPrice
+		}
+
+		totalRevenue += totalPrice
+		transactionsResponse[i] = model.TransactionResponse{
+			Id: transaction.Id,
+			ProductName: transaction.Product.Name,
+			TransactionType: transaction.TransactionType,
+			Quantity: transaction.Quantity,
+			Price:int64(transaction.Product.PricePerButir),
+			TotalPrice: int64(totalPrice),
+			TransactionDate: transaction.CreatedAt,
+		}
+	}
+
+	return model.GetTransaction{
+		TotalTransaction: int(totalRows),
+		TotalRevenue: totalRevenue,
+		TotalPurchase: totalPurchase,
+		Transactions: transactionsResponse,
+		TotalPages: totalPages,
+	}, nil, http.StatusOK
 }
