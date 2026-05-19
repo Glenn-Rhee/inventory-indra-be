755d259fd80b7db4bfb49006799982b59b5e1834
Glenn-Rhee <glennviktor5@gmail.com>
2026-05-19 10:09:09 +0700

    refactor: change layout struct

--- model/product.go
+++ model/product.go
@@ -4,7 +4,6 @@ import (
 	"time"
 )
 
-
 type ExpiredStatus string
 
 const (
@@ -16,57 +15,57 @@ const (
 type Category string
 
 const (
-	CategoryMedicine 	Category = "MEDICINE"
-	CategoryEssentials 	Category = "ESSENTIALS" 
+	CategoryMedicine   Category = "MEDICINE"
+	CategoryEssentials Category = "ESSENTIALS"
 )
 
 type Product struct {
-	Id				string 			`gorm:"primaryKey;type:varchar(255)"`
-	Name 			string 			`gorm:"not null"`
-	Category 		Category 		`gorm:"not null"`
-	PricePerButir	int 
-	ExpiredDate 	time.Time		`gorm:"not null"`
-	CreatedAt		time.Time
-	UpdatedAt		time.Time
-	DeletedAt		time.Time
-	IsActive		bool
+	Id            string   `gorm:"primaryKey;type:varchar(255)"`
+	Name          string   `gorm:"not null"`
+	Category      Category `gorm:"not null"`
+	PricePerButir int
+	ExpiredDate   time.Time `gorm:"not null"`
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     time.Time
+	IsActive      bool
 
-	Stock 			*Stock			`gorm:"foreignKey:ProductId;constraint:OnDelete:CASCADE"`
-	Transaction		[]Transaction	`gorm:"foreignKey:ProductId"`
+	Stock       *Stock        `gorm:"foreignKey:ProductId;constraint:OnDelete:CASCADE"`
+	Transaction []Transaction `gorm:"foreignKey:ProductId"`
 }
 
 type CreateProduct struct {
-	Name 			string		`json:"name" binding:"required,min=5,max=100"`
-	Category		Category	`json:"category" binding:"required,oneof=MEDICINE ESSENTIALS"`
-	Stock			int			`json:"stock" binding:"required,min=1"`
-	PricePerButir	int			`json:"price" binding:"required,min=100"`
-	ExpiredDate		time.Time	`json:"expiredDate" binding:"required"`
+	Name          string    `json:"name" binding:"required,min=5,max=100"`
+	Category      Category  `json:"category" binding:"required,oneof=MEDICINE ESSENTIALS"`
+	Stock         int       `json:"stock" binding:"required,min=1"`
+	PricePerButir int       `json:"price" binding:"required,min=100"`
+	ExpiredDate   time.Time `json:"expiredDate" binding:"required"`
 }
 
 type ProductsResponseGet struct {
-	TotalPages 	int64
-	Product	 	[]GetProducts
+	TotalPages int64
+	Product    []GetProducts
 }
 
 type GetProducts struct {
-	Id 				string
-	Name 			string
-	Category 		Category
-	Price			int
-	StatusExpired	ExpiredStatus
-	ExpiredDate		time.Time
-	Transactions 	[]TransactionProduct
+	Id            string
+	Name          string
+	Category      Category
+	Price         int
+	StatusExpired ExpiredStatus
+	ExpiredDate   time.Time
+	Transactions  []TransactionProduct
 }
 
 type TransactionProduct struct {
-	Month 			string
-	In				int
-	Out 			int
+	Month string
+	In    int
+	Out   int
 }
 
 type PatchProduct struct {
-	Id				string		`json:"id" binding:"required"`
-	Name			string		`json:"name" binding:"required,min=5,max=100"`
-	Category		Category	`json:"category" binding:"required,oneof=MEDICINE ESSENTIALS"`
-	PricePerButir	int			`json:"price" binding:"required,min=100"`
+	Id            string   `json:"id" binding:"required"`
+	Name          string   `json:"name" binding:"required,min=5,max=100"`
+	Category      Category `json:"category" binding:"required,oneof=MEDICINE ESSENTIALS"`
+	PricePerButir int      `json:"price" binding:"required,min=100"`
 }
