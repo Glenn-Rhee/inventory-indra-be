15c6bee891a0dcde2795b00fb9aa4cfaa597514f
Glenn-Rhee <glennviktor5@gmail.com>
2026-05-27 18:21:24 +0700

    feat: added allow origin

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://inventory-indra.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "Accept", "x-user-id"},
 		ExposeHeaders:    []string{"Content-Length", "Content-Disposition"},
