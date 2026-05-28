f1025ce279220811e7e2df97da3ac864d295e772
Glenn-Rhee <glennviktor5@gmail.com>
2026-05-28 11:00:57 +0700

    feate: added sslmode while local and hosting

--- db/config.go
+++ db/config.go
@@ -15,20 +15,23 @@ var DB *gorm.DB
 
 func Connect() *gorm.DB {
 	log.Println("Connecting to db...")
+	sslMode := "sslmode=require"
 	if _, err := os.Stat(".env"); err == nil {
 		err := godotenv.Load()
 		if err != nil {
 			log.Println("Error load dotenv file:", err.Error())
 		}
+		sslMode = "sslmode=disable"
 	}
 
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+		"host=%s user=%s password=%s dbname=%s port=%s %s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
+		sslMode,
 	)
 
 	db, err := gorm.Open(postgres.New(postgres.Config{
