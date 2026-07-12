9c152868da279457cc8a95486658ca0fa95ced47
Glenn-Rhee <glennviktor5@gmail.com>
2026-07-12 21:13:59 +0700

    refactor: refactoring stock.go

--- model/stock.go
+++ model/stock.go
@@ -4,38 +4,38 @@ import (
 	"time"
 )
 
+type Stock struct {
+	Id            string `gorm:"primaryKey;type:varchar(255)"`
+	StockPerButir int    `gorm:"not null"`
+	LastUpdate    time.Time
+	ProductId     string `gorm:"type:varchar(255)"`
+
+	Product *Product `gorm:"foreignKey:ProductId"`
+}
+
 type StatusStock string
 
 const (
-	StatusStockSafe 	StatusStock = "SAFE"
-	StatusStockLow		StatusStock	= "LOW-STOCK"
-	StatusStockSoldOut	StatusStock	= "SOLD-OUT"
+	StatusStockSafe    StatusStock = "SAFE"
+	StatusStockLow     StatusStock = "LOW-STOCK"
+	StatusStockSoldOut StatusStock = "SOLD-OUT"
 )
 
-type Stock struct {
-	Id 				string 		`gorm:"primaryKey;type:varchar(255)"`
-	StockPerButir	int 		`gorm:"not null"`
-	LastUpdate		time.Time
-	ProductId 		string		`gorm:"type:varchar(255)"`
-
-	Product 		*Product	`gorm:"foreignKey:ProductId"`
-}
-
 type GetStocks struct {
-	TotalProduct 		int64
-	TotalLowStock		int
-	TotalProductExpired	int
-	Products			[]GetProductStock			 
-	TotalPages		 	int		
+	TotalProduct        int64
+	TotalLowStock       int
+	TotalProductExpired int
+	Products            []GetProductStock
+	TotalPages          int
 }
 
 type GetProductStock struct {
-	Id				string
-	Name			string
-	Stock			int
-	Price 			int64
-	StatusStock		StatusStock
-	StatusExpired	ExpiredStatus
-	ExpiredDate		time.Time
-	LastUpdate		time.Time	
+	Id            string
+	Name          string
+	Stock         int
+	Price         int64
+	StatusStock   StatusStock
+	StatusExpired ExpiredStatus
+	ExpiredDate   time.Time
+	LastUpdate    time.Time
 }
